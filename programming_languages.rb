4fec2a31465de659b8fd9982762dd99b388b3070
Lola Onanuga <slonanuga@gmail.com>
2018-10-03 17:28:10 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,7 @@ def reformat_languages(languages)
    
     lang.each do |lang_sys,type|
      #binding.pry
-      new_hash[lang_sys] = {type.keys[0].join("") => type.values.join("")}
+      new_hash[lang_sys] = {type.keys[0] => type.values.join("")}
       
     end
     
