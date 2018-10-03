267c668a698ecedcb77c2d5e11720ae01250e5e0
Lola Onanuga <slonanuga@gmail.com>
2018-10-03 17:59:51 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+ 
+  new_hash = {}
+  
+  
+  languages.each do |lang_type,lang|
+   
+    lang.each do |lang_sys,type|
+     
+      if new_hash[lang_sys] == nil
+        
+        new_hash[lang_sys] = {type.keys[0] => type.values.join(""), :style => [lang_type]}
+      
+    else
+      
+        new_hash[lang_sys][:style] << lang_type
+        
+      
+      end
+      
+    end
+    
+  end
+    
+    new_hash
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+
 end
